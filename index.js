88ee95eaeba64f3153797ac5e1586418b368383c
djay <djay@boegismedia.com>
2022-04-18 23:44:37 +0700

    emit event, create endpoint to receive an event and use const for the port

--- index.js
+++ index.js
@@ -6,26 +6,39 @@ const { randomBytes } = require('crypto')
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+const axios = require('axios')
 
 const commentsByPostId = {};
+const eventBusEndpoint = 'http://localhost:4005/events'
 
 app.get('/posts/:id/comments', (req, res) => {
   const postId = req.params.id;
   res.send(commentsByPostId[postId] || [])
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const {content} = req.body;
   const postId = req.params.id;
   
   const comments = commentsByPostId[postId] || [];
-  comments.push({content, id: commentId});
+  const newComment = {content, id: commentId}
+  comments.push(newComment);
 
   commentsByPostId[postId] = comments;
+  
+  await axios.post(eventBusEndpoint, {type:'CommentCreated', data:{...newComment, postId}})
+
   res.status(201).send(comments)
 });
 
-app.listen(4001, () => {
-  console.log('Listening on 4001')
+app.post('/events', (req, res) => {
+  const {type} = req.body
+  console.log(`receiving ${type} event`)
+  res.send({})
+})
+
+const port = 4001
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
 });
