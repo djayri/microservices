4d305777638fedce58d08c11dbaca0ef7c1303f8
djay <djay@boegismedia.com>
2022-04-21 11:13:25 +0700

    emit commentupdated and handle commentModeration event

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { randomBytes } = require('crypto')
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-const axios = require('axios')
+const axios = require('axios');
 
 const commentsByPostId = {};
 const eventBusEndpoint = 'http://localhost:4005/events'
@@ -22,21 +22,52 @@ app.post('/posts/:id/comments', async (req, res) => {
   const postId = req.params.id;
   
   const comments = commentsByPostId[postId] || [];
-  const newComment = {content, id: commentId}
+  const newComment = { content, id: commentId, status:'pending' }
   comments.push(newComment);
 
   commentsByPostId[postId] = comments;
   
-  await axios.post(eventBusEndpoint, {type:'CommentCreated', data:{...newComment, postId}})
+  await axios.post(eventBusEndpoint, {
+    type:'CommentCreated', 
+    data: {
+      ...newComment, 
+      postId
+    }
+  })
 
   res.status(201).send(comments)
 });
 
-app.post('/events', (req, res) => {
-  const {type} = req.body
+app.post('/events', async (req, res) => {
+  const {type, data} = req.body
   console.log(`receiving ${type} event`)
+  switch(type) {
+    case 'CommentModerated':
+      await handleCommentModeratedEvent(data)
+      break;
+  }
   res.send({})
 })
+
+const handleCommentModeratedEvent = async (data) => {
+  const { postId, status, id } = data
+  if (!postId || !status) {
+    return
+  }
+  
+  const comment = commentsByPostId[postId].find(comment => comment.id === id)
+  comment.status = status;
+
+  await axios.post('http://localhost:4005/events', {
+    type:'CommentUpdated', 
+    data:{
+      postId,
+      id,
+      status,
+      content: comment.content,
+    }
+  })
+}
 
 const port = 4001
 app.listen(port, () => {
